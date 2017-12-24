4888643549e7f284c56f8d7005856abf9a147b80
Aidan Griffin <agriffin@stuy.edu>
2017-12-23 22:41:44 -0500

    added toString() Method

--- Function.java
+++ Function.java
@@ -3,25 +3,42 @@ public class Function{
     //initializes an ArrayList in which the even indexes are the coefficients and the odd indexes are the powers you are raising the polynomial to
     private ArrayList<Integer> polynomial;
     private int degree;//represents the maximum of the even exponents          
+
+    //Overridden Constructor
     public Function(int d, ArrayList a){
         degree = d;
         polynomial = a;
     }
-    //returns the ArrayList with the derivative of the polynomial              
+    //toString() method
+    public String toString(){
+        String ret = "";
+        for (int i = 0; i < (polynomial.size() / 2); i++){
+	    ret += polynomial.get(2 * i) + "x^" +  polynomial.get(2 * i + 1) + " + ";
+	}
+	ret = ret.substring(0, ret.length() - 2);
+	return ret;
+    }
+    //returns the ArrayList with the derivative of the polynomial          
     public ArrayList takeDerivative(){
-        ArrayList<Integer> ret = new ArrayList();
         for (int i = 0; i < polynomial.size(); i++){
             if (i % 2 == 0 && (i != (polynomial.size() - 2))){
-                ret.add(polynomial.get(i) * polynomial.get(i + 1));
+		//the coefficient after taking derivative of polynomial is the exponent times the original coefficient
+                polynomial.set(i, polynomial.get(i) * polynomial.get(i + 1));
             }
-            else if (i % 2 == 1){
+            else if (i % 2 == 1 && (i != (polynomial.size() - 1))){
                 if (polynomial.get(i) >= 1){
-                    ret.add(polynomial.get(i) - 1);
+		    //the degree gets lowered by one
+                    polynomial.set(i, polynomial.get(i) - 1);
                 }
 	    }
+	    else if (i == polynomial.size() - 2){
+		polynomial.remove(i);//a constant term is shaved off after taking the derivative of it
+		polynomial.remove(i);//the degree of the constant term is also shaved off
+	    }
 	}
-        return ret;
+	return polynomial;
     }
+    //test out methods
     public static void main(String[] args){
         ArrayList<Integer> function = new ArrayList();
         //we are trying to find the derivative of 2x^3 + 9x^2 + 6x + 7         
@@ -34,6 +51,9 @@ public class Function{
         function.add(7);
         function.add(0);
         Function x = new Function(3, function);
+	System.out.println(function);
+	System.out.println(x);//prints out the actual polynomial
         System.out.println(x.takeDerivative());//should return [6,2,18,1,6,0]  
+	System.out.println(x);
     }
 }

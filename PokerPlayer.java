5b7fff228a5c07ba43866cbc2060f7c77baed982
Woosuk Lee <wlee10@stuy.edu>
2018-01-09 04:37:26 -0500

    deleted evaluate() in PokerPlayer.java

--- PokerPlayer.java
+++ PokerPlayer.java
@@ -4,9 +4,6 @@ public class PokerPlayer extends Player{
 	super(n,s);
 	temp = new Card[s];
     }
-    public void evaluate(){
-	bubbleSort();
-    }
     public void bubbleSort(){
 	//copies elements of hand into temp
 	for (int k = 0; k < hand.length; k++){
